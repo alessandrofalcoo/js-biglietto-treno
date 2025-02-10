aba4caf489e0eaf2d687c705aa242dec0cbaee23
alessandrofalcoo <alefalco.1003@gmail.com>
2025-02-10 16:17:34 +0100

    add if statement with alerts

--- bonus/bonus.js
+++ bonus/bonus.js
@@ -8,10 +8,22 @@ Stabilire e comunicare chi ha vinto secondo le regole di pari o dispari.
 
 
 
-const user_choice = prompt("Inserisci se pari o dispari, [pari, dispari]");
+let user_choice = prompt("Inserisci se pari o dispari, [pari, dispari]");
 console.log(user_choice);
 
-const userNumber = parseInt(prompt("Inserire un valore numerico compreso tra 1 e 9 [1, 2, 3]"));
-console.log(userNumber);
+const user_Number = parseInt(prompt("Inserire un valore numerico compreso tra 1 e 9"));
+console.log(user_Number);
 
 const random_number = Math.floor(Math.random() * 9) + 1;
+console.log(random_number);
+
+
+if (user_choice = "pari" && user_Number % 2 !== 0) {
+    alert("ma se hai detto pari!");
+
+} else if (user_choice = "dispari" && user_Number % 2 !== 0) {
+    alert("ma se hai detto dispari!");
+
+} else {
+    alert("bravo!");
+}
