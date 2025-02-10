39044b97c56451d6c1709a511f381c38e89b48fb
alessandrofalcoo <alefalco.1003@gmail.com>
2025-02-10 17:01:37 +0100

    fix even or odd add date html and js ex

--- bonus/even-or-odd.js
+++ bonus/even-or-odd.js
@@ -14,16 +14,20 @@ console.log(user_choice);
 const user_Number = parseInt(prompt("Inserire un valore numerico compreso tra 1 e 9"));
 console.log(user_Number);
 
-const random_number = Math.floor(Math.random() * 9) + 1;
+let random_number = Math.floor(Math.random() * 9) + 1;
 console.log(random_number);
 
+const somma = user_Number + random_number;
+console.log(somma)
 
-if (user_choice = "pari" && user_Number % 2 !== 0) {
-    alert("ma se hai detto pari!");
+ if (user_choice == "pari" && somma % 2 !== 0) {
+    alert("hai perso sfigato!");
 
-} else if (user_choice = "dispari" && user_Number % 2 !== 0) {
-    alert("ma se hai detto dispari!");
+} else if (user_choice == "dispari" && somma % 2 === 0) {
+    alert("hai perso");
 
 } else {
-    alert("bravo!");
+    alert("hai vinto, bravissimo!");
 }
+
+
